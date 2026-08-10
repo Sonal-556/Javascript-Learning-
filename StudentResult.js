174f34e264b7172ecfc58084e55d9295877a5cc9
Sonal <example@gmail.com>
2026-08-10 21:20:46 +0530

    Added Student Result Program

---
+++ StudentResult.js
@@ -0,0 +1,11 @@
+let name = "Sonal";
+let marks = 75;
+
+console.log("Student Name:", name);
+console.log("Marks:", marks);
+
+if (marks >= 80) {
+    console.log("Result: Pass");
+} else {
+    console.log("Result: Fail");
+}
