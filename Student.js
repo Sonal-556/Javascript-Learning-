3d9ae9a3a9a982a990bc01b1b81fe12b174861ae
Sonal <example@gmail.com>
2026-08-11 14:15:28 +0530

    Added Studnet Details Program

---
+++ Student.js
@@ -0,0 +1,10 @@
+let studnet = {
+    name : "Sonal",
+    age: 19,
+    course:"IT",
+    city: "Mumbai"
+}
+console.log("Name:", studnet.name);
+console.log("Age:", studnet.age);
+console.log("Course:", studnet.course);
+console.log("City:", studnet.city);
